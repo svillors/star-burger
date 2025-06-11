d4f35a5aa3fe700f2539f427d4f9175d2a1108d8
svillors <tntzator9@gmail.com>
2025-06-11 19:02:54 +0000

    Correct path to envitonment

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ if [ ! -d "venvasd" ]; then
 fi
 venvasd/bin/pip install -r requirements.txt
 npm ci --dev
-python3 manage.py migrate
+venvasd/bin/python3 manage.py migrate
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-python3 manage.py collectstatic --noinput
+venvasd/bin/python3 manage.py collectstatic --noinput
 sudo systemctl reload nginx.service
 sudo systemctl restart star-burger.service
 source .env
