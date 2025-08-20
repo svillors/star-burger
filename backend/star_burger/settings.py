19b8c84fec71f69ffd6dff37a73ccc26469efecd
svillors <tntzator9@gmail.com>
2025-08-21 00:03:56 +0500

    Move requirements to backend and change paths in settings

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -129,7 +129,7 @@ INTERNAL_IPS = [
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
-    os.path.join(BASE_DIR, "bundles"),
+    os.path.join(BASE_DIR, '..', 'frontend', "bundles"),
 ]
 
 ROLLBAR = {
